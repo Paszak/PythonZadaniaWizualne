2ea97264afc796e3b2a26b790dfd0ee85328f741
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-03-28 16:59:42 +0200

    zad 2 add only numbers in inputs

--- kalkulatorek.py
+++ kalkulatorek.py
@@ -8,42 +8,54 @@ okno.title("Kalkulatorek")
 
 def Oblicz():
     global wyświetlaczStr1, wyświetlaczStr2, r1
-    lewaWar= int(wyświetlaczStr1.get())
-    prawaWar = int(wyświetlaczStr2.get())
-    print(lewaWar, prawaWar, r1.get()) #sprawdzam co otrzymuję
+    lewa_war= int(wyświetlaczStr1.get())
+    prawa_war = int(wyświetlaczStr2.get())
+    print(lewa_war, prawa_war, r1.get()) #sprawdzam co otrzymuję
     if r1.get() == 0:
-        wynik = lewaWar + prawaWar
+        wynik = lewa_war + prawa_war
     elif r1.get() == 1:
-        wynik = lewaWar - prawaWar
+        wynik = lewa_war - prawa_war
     elif r1.get() == 2:
-        if prawaWar == 0:
+        if prawa_war == 0:
             messagebox.showinfo("Błąd", "Nie można dzielić przez zero")
             wynik = False
         else:
-            wynik = lewaWar / prawaWar
+            wynik = lewa_war / prawa_war
     elif r1.get() == 3:
-        wynik = lewaWar * prawaWar
+        wynik = lewa_war * prawa_war
     if wynik:
         messagebox.showinfo("Wynik", 'Wynikiem działania jest: ' + str(wynik))
 
 def tylkocyfry(*a):
-    global wyświetlaczStr1, pop_s
+    global wyświetlaczStr1, pop_s1
     s = wyświetlaczStr1.get()
     if s == '' or s.isdigit():
-        pop_s = s
+        pop_s1 = s
     else:
-        wyświetlaczStr1.set(pop_s)
+        wyświetlaczStr1.set(pop_s1)
 
-pop_s = ''
+def tylkocyfry2(*a):
+    global wyświetlaczStr2, pop_s2
+    s = wyświetlaczStr2.get()
+    if s == '' or s.isdigit():
+        pop_s2 = s
+    else:
+        wyświetlaczStr2.set(pop_s2)
+
+pop_s1 = ''
+pop_s2 = ''
+
 wyświetlaczStr1 = StringVar()
 wyświetlaczStr1.set("")
 wyświetlaczStr2 = StringVar()
 wyświetlaczStr2.set("")
 
-wyświetlaczStr1.set(pop_s)
+wyświetlaczStr1.set(pop_s1)
 wyświetlaczStr1.trace('w', tylkocyfry)
-tc = okno.register(tylkocyfry)
 
+wyświetlaczStr2.set(pop_s2)
+wyświetlaczStr2.trace('w', tylkocyfry2)
+tc = okno.register(tylkocyfry)
 
 wyświetlacz1 = Entry(okno, width=5, font=("Courier New", "12", "bold"), textvariable=wyświetlaczStr1, justify=LEFT)
 wyświetlacz2 = Entry(okno, width=5, font=("Courier New", "12", "bold"), textvariable=wyświetlaczStr2, justify=LEFT)
